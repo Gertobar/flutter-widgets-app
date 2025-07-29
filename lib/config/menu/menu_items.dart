f780b2d5f137bc217d3b39917c4073c465bffead
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 <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>
2025-07-28 23:44:49 -0500

    snackbars dialogs terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -30,9 +30,16 @@ const appMenuItem = <MenuItem>[
   ),
 
   MenuItem(
-    title: 'ProgressIndicators',
+    title: 'Progress Indicators',
     subTitle: 'Generales y controlados',
     link: '/progress',
     icon: Icons.refresh_rounded,
+  ),
+
+    MenuItem(
+    title: 'Snackbars and Dialogs',
+    subTitle: 'Indicadores en pantalla',
+    link: '/snackbars',
+    icon: Icons.info_outline,
   ),
 ];
--- lib/config/router/app_roouter.dart
+++ lib/config/router/app_roouter.dart
@@ -1,8 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:widgests_app/features/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widgests_app/features/presentation/screens/cards/cards_screen.dart';
-import 'package:widgests_app/features/presentation/screens/home/home_screen.dart' show HomeScreen;
+import 'package:widgests_app/features/presentation/screens/home/home_screen.dart'show HomeScreen;
 import 'package:widgests_app/features/presentation/screens/progress/progress_screen.dart';
+import 'package:widgests_app/features/presentation/screens/screens.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
@@ -19,17 +20,23 @@ final appRouter = GoRouter(
       name: ButtonsScreen.name,
       builder: (context, state) => ButtonsScreen(),
     ),
-    
+
     GoRoute(
       path: '/cards',
       name: CardsScreen.name,
       builder: (context, state) => CardsScreen(),
     ),
 
-        GoRoute(
+    GoRoute(
       path: '/progress',
       name: ProgressScreen.name,
       builder: (context, state) => ProgressScreen(),
+    ),
+
+        GoRoute(
+      path: '/snackbars',
+      name: SnackbaarScreen.name,
+      builder: (context, state) => SnackbaarScreen(),
     ),
   ],
 );
